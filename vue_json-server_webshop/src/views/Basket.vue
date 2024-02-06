064252827f9cf6d4a5ed14e3fe85632d40bffb7d
ZakarBalint <szt2zakbal@vasvari.org>
2024-02-06 09:37:04 +0100

    asd

--- vue_json-server_webshop/src/views/Basket.vue
+++ vue_json-server_webshop/src/views/Basket.vue
@@ -1,9 +1,29 @@
 <script setup>
+  import axios from 'axios';
+  import {ref, onMounted} from 'vue';
 
+  const basket = ref([]);
+
+  onMounted(() => {
+    getBasket();
+  })
+
+  const getBasket = () => {
+    axios.get("http://localhost:3000/cart")
+    .then(resp => basket.value = resp.data);
+  }
+
+  const removeFromBasket = (item) => {
+    axios.delete("http://localhost:3000/cart/" + item.id)
+    .then(getBasket());
+  }
 </script>
 
 <template>
   <div>
     <h1>Kosár</h1>
+    <div>
+      <div v-for="item in basket" @click="removeFromBasket(item)">{{ item.name }}</div>
+    </div>
   </div>
 </template>
--- vue_json-server_webshop/src/views/Shop.vue
+++ vue_json-server_webshop/src/views/Shop.vue
@@ -1,5 +1,32 @@
+<script setup>
+  import axios from 'axios';
+  import {ref, onMounted} from 'vue';
+
+  const items = ref([]);
+
+  onMounted(() => {
+    axios.get("http://localhost:3000/products")
+    .then(resp => items.value = resp.data);
+  })
+
+  const addToBasket = (item) => {
+    console.log(item);
+
+    axios.post("http://localhost:3000/cart",{
+      id : item.id,
+      name : item.name, 
+      price: item.price, 
+      img : item.img,
+    })
+  }
+</script>
+
 <template>
   <div>
     <h1>Bolt</h1>
+
+    <div>
+      <div v-for="item in items" @click="addToBasket(item)">{{ item.name }}</div>
+    </div>
   </div>
 </template>
